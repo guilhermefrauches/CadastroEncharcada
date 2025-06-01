14aff2781b244e2d75be14bcf9f77312ac67634d
Guilherme Moreira <90403785+guilhermefrauches@users.noreply.github.com>
2025-05-31 21:07:30 -0300

    adição de outros validadores
    nesse comit fiz adição de validadores como: email, CEP e entre outros

--- validadores.py
+++ validadores.py
@@ -14,5 +14,27 @@ def validar_cpf(cpf):
             digito = 0
         if digito != int(cpf[i]):
             return False
-
     return True
+
+
+def validar_telefone(telefone):
+
+    telefone = ''.join(filter(str.isdigit, telefone))
+
+    if len(telefone) == 10:
+        return True
+    elif len(telefone) == 11:
+        if telefone[2] == '9':
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def validar_cep(cep):
+    cep = ''.join(filter(str.isdigit, cep))
+    return len(cep) == 8
+
+def validar_nome_completo(nome):
+    partes = nome.strip().split()
+    return len(partes) >= 2
